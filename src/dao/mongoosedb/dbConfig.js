d3d3f231914592020ca6595d61fb0d917ae9e1ee
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-09-25 06:49:45 -0600

    inicio de diA

--- src/dao/mongoosedb/dbConfig.js
+++ src/dao/mongoosedb/dbConfig.js
@@ -1,7 +1,7 @@
 import  mongoose  from "mongoose"
 
 // const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/7Entregabk?retryWrites=true&w=majority"
-const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/7Entregabk?retryWrites=true&w=majority"
+const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/8Entregabk?retryWrites=true&w=majority"
 
 mongoose.connect(URI,{ useNewUrlParser: true,
     useUnifiedTopology: true})
